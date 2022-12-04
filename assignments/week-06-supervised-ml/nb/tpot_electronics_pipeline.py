f8c4ba8105b3b6fcf534a0a84da0f6761d6c982d
safia-bashir <safia.bashir332@gmail.com>
2022-12-04 10:29:35 +0400

    week 6

--- assignments/week-06-supervised-ml/nb/tpot_electronics_pipeline.py
+++ assignments/week-06-supervised-ml/nb/tpot_electronics_pipeline.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
-from sklearn.decomposition import FastICA
-from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import ExtraTreesClassifier, RandomForestClassifier
+from sklearn.feature_selection import RFE
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
 from tpot.export_utils import set_param_recursive
@@ -12,10 +12,10 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=42)
 
-# Average CV score on the training set was: 1.0
+# Average CV score on the training set was: 0.9998000000000001
 exported_pipeline = make_pipeline(
-    FastICA(tol=0.30000000000000004),
-    ExtraTreesClassifier(bootstrap=True, criterion="entropy", max_features=0.9000000000000001, min_samples_leaf=2, min_samples_split=5, n_estimators=100)
+    RFE(estimator=ExtraTreesClassifier(criterion="entropy", max_features=0.7000000000000001, n_estimators=100), step=0.7500000000000001),
+    RandomForestClassifier(bootstrap=False, criterion="entropy", max_features=0.6500000000000001, min_samples_leaf=1, min_samples_split=17, n_estimators=100)
 )
 # Fix random state for all the steps in exported pipeline
 set_param_recursive(exported_pipeline.steps, 'random_state', 42)
